a9167e7430737a7b0cb0677d3b0c64b8f70dc51b
WictorNisa <wictornisa@hotmail.com>
2021-02-27 18:55:38 +0100

    Added new behavior to the app

--- js/app.js
+++ js/app.js
@@ -4,12 +4,24 @@ const quoteContainer = document.getElementById('quote-div');
 const quoteh6 = document.getElementById('quote-h6');
 const quotep = document.getElementById('quote-p');
 const locationText = document.getElementById('locationText');
+const currentTimezone = document.getElementById('current-timezone-p');
+const dayOfYear = document.getElementById('dayofyear-p');
+const dayOfWeek = document.getElementById('dayofweek-p');
+const currentWeekNumber = document.getElementById('weeknumber-p');
+const buttonDiv = document.getElementById('modal-show');
+const button = document.getElementById('button');
+
+
+
 
 function loadTime() {
+  const greetUserText = document.getElementById('greet-the-user');
+  const main = document.getElementById('main');
   const loadPromise = fetch("http://worldtimeapi.org/api/ip");
   loadPromise.then((response) => {
     const convertResponse = response.json();
     convertResponse.then((timezone) => {
+        // console.log(timezone);
       let localTime = timezone.datetime;
       const date = new Date(localTime);
       let hours = date.getHours();
@@ -22,9 +34,26 @@ function loadTime() {
       hours = addZero(hours);
       minutes = addZero(minutes);
 
-
-      let timeString = `${hours} : ${minutes}`;
+      let timeString = `${hours}:${minutes}`;
       timeContainer.innerHTML = timeString;
+
+      function checkTime() {
+          if(hours <= 12){
+            greetUserText.innerText = `Good Morning`;
+          } else if (hours <= 18){
+            greetUserText.innerText = `Good Afternoon`;
+          } else {
+            greetUserText.innerText = `Good Evening`;
+            main.style.backgroundImage = "url('../img/night-hero.jpg')";
+          }
+    }
+    checkTime();
+      let currentDayOfYear = `${timezone.day_of_year}`;
+      dayOfYear.innerText = currentDayOfYear;
+      let currentDayOfWeek = `${timezone.day_of_week}`;
+      dayOfWeek.innerText = currentDayOfWeek;
+      let currentWeek = `${timezone.week_number}`;
+      currentWeekNumber.innerText = currentWeek;
       let timer = setTimeout(loadTime, 1000);
     });
   });
@@ -55,9 +84,44 @@ function loadLocation() {
     loadPromise.then(response => {
         const convertResponse = response.json();
         convertResponse.then((location) =>{
-            let locationBody = `${location.country_name}, ${location.country_code}`;
+            let locationBody = `IN ${location.country_name}, ${location.country_code}`;
             locationText.innerText = locationBody;
+            let currentTimeText = `${location.time_zone}`;
+            currentTimezone.innerText = currentTimeText;
         });
     });
 }
 loadLocation();
+
+
+button.addEventListener("click", () => {
+  const moreLessText = document.getElementById('more-less-p');
+  const modal = document.getElementById('modal-flip');
+    if(modal.style.display === 'none'){
+      modal.style.display = 'flex';
+      moreLessText.innerText = 'More';
+    } else{
+      modal.style.display = 'none';
+      moreLessText.innerText = 'More';
+
+    }
+});
+
+
+function loadWeather() {
+  const weatherP = document.getElementById('weather-p');
+  const loadPromise = fetch('http://api.weatherapi.com/v1/current.json?key=735b3b6062174bf08f8155048212602&q=Lund');
+  loadPromise.then(response => {
+    const convertResponse = response.json();
+    convertResponse.then((weather) =>{
+        console.log(weather.current);
+        let weatherbody = `Currently ${weather.current.temp_c}c`;
+        weatherP.innerText = weatherbody;
+        
+
+    });
+});
+}
+loadWeather();
+
+
